d598f9c3e59e0604b0668f279e1b84bbefa52a97
Amy Spears <amspears007@gmail.com>
2023-08-05 13:20:23 -0600

    Add service tests for keys, class type and how many recipes

---
+++ spec/services/recipe_service_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe RecipeService do
+  describe "it gets all recipes" do
+    it "gets recipes from a specific country" do
+      recipes = RecipeService.all_recipes("thailand")
+      expect(recipes[:hits]).to be_an(Array)
+      expect(recipes[:hits].count).to eq(10)
+
+      recipes[:hits].each do |recipe|
+        expect(recipe[:recipe]).to have_key(:uri)
+        expect(recipe[:recipe]).to have_key(:label)
+        expect(recipe[:recipe]).to have_key(:image)
+
+
+        # require 'pry'; binding.pry
+
+      end
+    end
+  end
+end
