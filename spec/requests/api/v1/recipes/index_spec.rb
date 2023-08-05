fc55a88126fca865e1630eab137fe8d43c894999
Amy Spears <amspears007@gmail.com>
2023-08-05 12:50:01 -0600

    Add routes recipes index route and update query in get request in spec

--- spec/requests/api/v1/recipes/index_spec.rb
+++ spec/requests/api/v1/recipes/index_spec.rb
@@ -1,9 +1,8 @@
 require "rails_helper"
 
-describe "Recipes index request" do
+RSpec.describe "Recipes index request" do
   it "requests all the recipes" do
     get "/api/v1/recipes?country=thailand"
-    get api_v1_users_path
     expect(response).to be_successful
 
     users = JSON.parse(response.body, symbolize_names: true)
