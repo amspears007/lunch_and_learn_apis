3759ade3f178dae76055c7d33a18dcffdccf6532
Amy Spears <amspears007@gmail.com>
2023-08-05 13:19:15 -0600

    Add Recipe Service with self.conn and self.all_recipes method

---
+++ app/services/recipe_service.rb
@@ -0,0 +1,14 @@
+class RecipeService
+  def self.conn
+    Faraday.new("https://api.edamam.com") do |faraday|
+    faraday.params["app_id"] = ENV["app_id"]
+    faraday.params["app_key"] = ENV["app_key"]
+    end
+  end
+
+  def self.all_recipes(country)
+    response = conn.get("/search?q=#{country}")
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
