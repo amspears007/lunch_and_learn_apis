e3093eeb4883ce69d4ffd7cfe9a8ef202553e267
Amy Spears <amspears007@gmail.com>
2023-08-05 15:35:22 -0600

    Add poros test spec with Recipe.new with two arguments from nested attributes in initialize

---
+++ spec/poros/recipe_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe Recipe do
+  describe "it creates recipe objects" do
+    it "exists" do
+      attributes = {
+        recipe: {
+          label: "Amy's Deliciousness",
+          url: "https://www.amy's eats.com",
+          image: "funny food image"
+        }
+      }
+      country = "usa"
+
+      new_recipe = Recipe.new(attributes, country)
+      expect(new_recipe.country).to eq("usa")
+      expect(new_recipe.title).to eq("Amy's Deliciousness")
+      expect(new_recipe.url).to eq("https://www.amy's eats.com")
+      expect(new_recipe.image).to eq("funny food image")
+    end
+  end
+end
