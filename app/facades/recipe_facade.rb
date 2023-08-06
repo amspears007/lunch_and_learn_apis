cbce13dabb6d5cbd9f6e88013beb66accf25bc39
Amy Spears <amspears007@gmail.com>
2023-08-05 19:24:25 -0600

    add conditional to recipie facade if no recipes return and add several itblocks of testing

--- app/facades/recipe_facade.rb
+++ app/facades/recipe_facade.rb
@@ -1,9 +1,13 @@
 class RecipeFacade
   def self.get_recipes(country)
     response = RecipeService.all_recipes_by_country(country)
-
-    recipes = response[:hits].map do |recipe|
-      Recipe.new(recipe, country)
+# require 'pry'; binding.pry
+    if response[:hits].empty?
+      return []
+    else
+      recipes = response[:hits].map do |recipe|
+        Recipe.new(recipe, country)
+      end
     end
   end
 end
--- spec/requests/api/v1/recipes/index_spec.rb
+++ spec/requests/api/v1/recipes/index_spec.rb
@@ -37,5 +37,31 @@ RSpec.describe "Recipes index request" do
       expect(recipe[:attributes].keys).to eq([:title, :url, :country, :image, :id])
       end
     end
+
+    it "returns an empty array when no recipies match the given country" do
+      get "/api/v1/recipes?country=xyz"
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      recipe_result = JSON.parse(response.body, symbolize_names: true)
+
+      expect(recipe_result).to be_a(Hash)
+      expect(recipe_result).to have_key(:data)
+      expect(recipe_result[:data]).to eq([])
+    end
+
+    it "returns an empty array when the country inputted is an empty string" do
+      get "/api/v1/recipes?country="""
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      recipe_result = JSON.parse(response.body, symbolize_names: true)
+
+      expect(recipe_result).to be_a(Hash)
+      expect(recipe_result).to have_key(:data)
+      expect(recipe_result[:data]).to eq([])
+    end
   end
 end
