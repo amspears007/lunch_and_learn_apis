9ea9e6ecc57410de38c3202b662afc4eea5a4779
Amy Spears <amspears007@gmail.com>
2023-08-05 19:33:07 -0600

    Complete 1st endpoint US add get_url method to recipe service.

--- app/facades/recipe_facade.rb
+++ app/facades/recipe_facade.rb
@@ -1,7 +1,6 @@
 class RecipeFacade
   def self.get_recipes(country)
     response = RecipeService.all_recipes_by_country(country)
-# require 'pry'; binding.pry
     if response[:hits].empty?
       return []
     else
--- app/services/recipe_service.rb
+++ app/services/recipe_service.rb
@@ -1,14 +1,18 @@
 class RecipeService
+
+  def self.all_recipes_by_country(country)
+    get_url("/search?q=#{country}")
+  end
+
+  def self.get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   def self.conn
     Faraday.new("https://api.edamam.com") do |faraday|
     faraday.params["app_id"] = ENV["app_id"]
     faraday.params["app_key"] = ENV["app_key"]
     end
-  end
-
-  def self.all_recipes_by_country(country)
-    response = conn.get("/search?q=#{country}")
-
-    JSON.parse(response.body, symbolize_names: true)
   end
 end
