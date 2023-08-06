87d4fe1bc03ae456ede73a46e8d747566eb58be2
Amy Spears <amspears007@gmail.com>
2023-08-06 16:17:20 -0600

    change the request index spec to search spec and update in controller

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::RecipesController < ApplicationController
-  def index
+  def search
     country = CountryFacade.get_country(params[:country])
     recipes = RecipeFacade.get_recipes(country)
     render json: RecipeSerializer.new(recipes)
