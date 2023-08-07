374cad568721cd87caa3a6243480db77ddf33624
Amy Spears <amspears007@gmail.com>
2023-08-07 09:07:59 -0600

    Add request spec

---
+++ spec/requests/api/v1/airquality/search_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe "Airquality search request" do
+  describe "GET /api/v1/air_quality?country=#{country}" do
+    it "requests airquality of a specific city" do
+      get "/api/v1/air_quality?country=Nigeria"
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      result = JSON.parse(response.body, symbolize_names: true)
