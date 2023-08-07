85107f6103362f9ada4aebd4ede6a4ee9882d4fe
Amy Spears <amspears007@gmail.com>
2023-08-06 21:30:18 -0600

    update params in get_videos_by_country b/c snippet wasn't being called

--- app/services/video_service.rb
+++ app/services/video_service.rb
@@ -6,12 +6,16 @@ class VideoService
   # require 'pry'; binding.pry
   end
 
+  def self.get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   def self.get_videos_by_country(country)
-    response =conn.get('/youtube/v3/search') do |faraday|
-      faraday.params['part']= 'snippet'
-      faraday.params['q']= country
+    get_url("/youtube/v3/search?part=snippet&q=#{country}") do |faraday|
+      # faraday.params['part']= 'snippet'
+      # faraday.params['q']= country
     end
-    # require 'pry'; binding.pry
   end
 end
 
