30a085647b416a35771a2c73813390780ada324b
Amy Spears <amspears007@gmail.com>
2023-08-08 10:34:50 -0600

    add validation to generate api key with securerandom in private method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
+  # before_create :generate_api_key
+
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password
+  validates :password_digest, presence: true
+  validates :api_key, uniqueness: true
+  before_validation :generate_api_key
 
   has_secure_password
+
+  private
+
+  def generate_api_key
+    self.api_key = SecureRandom.base58(24)
+  end
 end
