90b5cd848474d5b3c8a97308bb818477d5799e92
Amy Spears <amspears007@gmail.com>
2023-08-07 20:04:19 -0600

    Add additional tests for JSON object returned

--- spec/requests/api/v1/learning_resources/search_spec.rb
+++ spec/requests/api/v1/learning_resources/search_spec.rb
@@ -7,8 +7,20 @@ RSpec.describe "Learning Resources Search Request" do
 
       expect(response).to be_successful
       expect(response.status).to eq(200)
-    video_result = JSON.parse(response.body, symbolize_names: true)
-    require 'pry'; binding.pry
+    result = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(result).to be_a(Hash)
+    expect(result[:data][:type]).to eq("learning_resource")
+    expect(result[:data][:id]).to eq(nil)
+    expect(result[:data][:attributes][:country]).to eq("thailand")
+    expect(result[:data][:attributes][:images]).to be_an(Array)
+    expect(result[:data][:attributes][:images].size).to be_a(Integer)
+    expect(result[:data][:attributes][:images].size).to eq(10)
+    expect(result[:data][:attributes][:images][0]).to have_key(:alt_tag)
+    expect(result[:data][:attributes]).to have_key(:video)
+    expect(result[:data][:attributes][:video]).to have_key(:video_id)
+
+
     end
   end
 end
