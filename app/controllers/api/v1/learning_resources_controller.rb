60b3204052b8eeae1e681eb7d9ee40edd5fc9682
Amy Spears <amspears007@gmail.com>
2023-08-07 20:48:43 -0600

    add tests to video facade and update tests in request spec and resources in controller

--- app/controllers/api/v1/learning_resources_controller.rb
+++ app/controllers/api/v1/learning_resources_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::LearningResourcesController < ApplicationController
   def search
-    resources = LearningResourceFacade.new.get_resources(params[:country])
+    resources = LearningResourceFacade.get_resources(params[:country])
     render json: LearningResourceSerializer.new(resources)
   end
 end
--- spec/facades/video_facade_spec.rb
+++ spec/facades/video_facade_spec.rb
@@ -1,9 +1,13 @@
 require "rails_helper"
 
 RSpec.describe VideoFacade do
-  it "returns a video"  do
+  it "returns a video object"  do
     video = VideoFacade.get_one_video("thailand")
 
-    expect(video).to be_an(Array)
+    expect(video).to be_a(Video)
+    expect(video.id).to eq(nil)
+    expect(video.title).to be_a(String)
+    expect(video.title).to eq("McDonald&#39;s in Thailand is NOT like in America 🇹🇭 (Ham &amp; Cheese Pie)")
+    expect(video.video_id).to be_a(String)
   end
 end
